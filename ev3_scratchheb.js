d99d154018c9b63ab57c5c270413674ce907bbe4
tzafonetev3 <mos@tzafonet.org.il>
2017-07-07 21:27:55 +0300

    Update ev3_scratchheb.js

--- ev3_scratchheb.js
+++ ev3_scratchheb.js
@@ -1472,7 +1472,7 @@ function(ext)
               [" ", "הפעל ממוע %m.whichMotorPort מהירות %n",              "startMotors",      "B+C", 100],
               [" ", "הפעל מנוע %m.whichMotorPort במהירות %n בכמות של %n מעלות ואז %m.brakeCoast",              "motorDegrees",      "A", 100, 360, "brake"],
               [" ", "עצור מנועים %m.whichMotorPort %m.brakeCoast",                       "motorsOff",     "כל המנועים", "brake"],
-              [" ", "הדלק נורה %m.patterns",                                 "setLED",                 "ירוק"],
+              [" ", "הדלק נורה %m.patterns",                                 "setLED",                 "בצבע ירוק"],
               ["h", "when button pressed on port %m.whichInputPort",       "whenButtonPressed","1"],
               ["h", "when IR remote %m.buttons pressed port %m.whichInputPort", "whenRemoteButtonPressed","Top Left", "1"],
               ["R", "button pressed %m.whichInputPort",                    "readTouchSensorPort",   "1"],
